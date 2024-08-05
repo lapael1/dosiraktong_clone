97ee5f4bfa9bb31a085d335c437be8b19cb9a99d
lapael1 <lapael6@gmail.com>
2024-08-05 15:57:49 +0900

    탑버튼 , 반응형 설정

--- js/business.js
+++ js/business.js
@@ -1,15 +1,19 @@
 window.addEventListener("load", function () {
-    const swBesiness = new Swiper(".sw-business", {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
+  const swBesiness = new Swiper(".sw-business", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
       },
-    });
+      1024: {
+        slidesPerView: 3,
+      },
+    },
   });
-  
+  // business-modal 기능
+  const businessModal = this.document.querySelector(".business-modal")
+  businessModal.addEventListener("click", function(){
+    businessModal.style.display = "none"
+  })
+});
